60aaadd8e8bb5cdfdccbc64706de6383141ca1ac
William Woodruff <william@tuffbizz.com>
2014-11-15 17:51:24 -0500

    more reformatting

--- src/bsdiff.c
+++ src/bsdiff.c
@@ -39,21 +39,21 @@ static void split(off_t *I, off_t *V, off_t start, off_t len, off_t h)
 {
 	off_t i, j, k, x, tmp, jj, kk;
 
-	if(len < 16)
+	if (len < 16)
 	{
-		for(k = start; k < (start + len); k += j)
+		for (k = start; k < (start + len); k += j)
 		{
 			j = 1;
 			x = V[I[k] + h];
 
-			for(i = 1; (k + i) < (start + len); i++)
+			for (i = 1; (k + i) < (start + len); i++)
 			{
-				if(V[I[k + i] + h] < x)
+				if (V[I[k + i] + h] < x)
 				{
 					x = V[I[k + i] + h];
 					j = 0;
 				}
-				if(V[I[k + i] + h] == x)
+				if (V[I[k + i] + h] == x)
 				{
 					tmp = I[k + j];
 					I[k + j] = I[k + i];
@@ -62,24 +62,24 @@ static void split(off_t *I, off_t *V, off_t start, off_t len, off_t h)
 				}
 			}
 
-			for(i = 0; i < j; i++)
+			for (i = 0; i < j; i++)
 				V[I[k + i]] = k + j - 1;
-			if(j == 1)
+			if (j == 1)
 				I[k] = -1;
 		}
 
 		return;
 	}
 
-	x=V[I[start + len / 2] + h];
-	jj=0;
-	kk=0;
+	x = V[I[start + len / 2] + h];
+	jj = 0;
+	kk = 0;
 
-	for(i = start; i < start + len; i++)
+	for (i = start; i < start + len; i++)
 	{
-		if(V[I[i] + h] < x)
+		if (V[I[i] + h] < x)
 			jj++;
-		if(V[I[i] + h] == x)
+		if (V[I[i] + h] == x)
 			kk++;
 	}
 
@@ -90,13 +90,13 @@ static void split(off_t *I, off_t *V, off_t start, off_t len, off_t h)
 	j = 0;
 	k = 0;
 
-	while(i < jj)
+	while (i < jj)
 	{
-		if(V[I[i] + h] < x)
+		if (V[I[i] + h] < x)
 		{
 			i++;
 		} 
-		else if(V[I[i] + h] == x)
+		else if (V[I[i] + h] == x)
 		{
 			tmp = I[i];
 			I[i] = I[jj + j];
@@ -112,9 +112,9 @@ static void split(off_t *I, off_t *V, off_t start, off_t len, off_t h)
 		}
 	}
 
-	while(jj + j < kk)
+	while (jj + j < kk)
 	{
-		if(V[I[jj + j] + h] == x)
+		if (V[I[jj + j] + h] == x)
 		{
 			j++;
 		}
@@ -127,16 +127,16 @@ static void split(off_t *I, off_t *V, off_t start, off_t len, off_t h)
 		}
 	}
 
-	if(jj > start)
+	if (jj > start)
 		split(I, V, start, (jj-start), h);
 
-	for(i=0; i < (kk-jj); i++)
+	for (i=0; i < (kk-jj); i++)
 		V[I[jj + i]] = kk - 1;
 
-	if(jj == kk - 1)
+	if (jj == kk - 1)
 		I[jj] = - 1;
 
-	if(start + len > kk)
+	if (start + len > kk)
 		split(I, V, kk, start + len - kk, h);
 }
 
@@ -145,47 +145,47 @@ static void qsufsort(off_t *I, off_t *V, uint8_t *old, off_t oldsize)
 	off_t buckets[256];
 	off_t i, h, len;
 
-	for(i = 0; i < 256; i++)
+	for (i = 0; i < 256; i++)
 		buckets[i] = 0;
-	for(i = 0;i < oldsize; i++)
+	for (i = 0;i < oldsize; i++)
 		buckets[old[i]]++;
-	for(i = 1;i < 256; i++)
+	for (i = 1;i < 256; i++)
 		buckets[i] += buckets[i - 1];
-	for(i = 255; i > 0; i--)
+	for (i = 255; i > 0; i--)
 		buckets[i] = buckets[i - 1];
 
 	buckets[0] = 0;
 
-	for(i = 0; i < oldsize; i++)
+	for (i = 0; i < oldsize; i++)
 		I[++buckets[old[i]]] = i;
 
 	I[0] = oldsize;
 
-	for(i = 0; i < oldsize; i++)
+	for (i = 0; i < oldsize; i++)
 		V[i] = buckets[old[i]];
 
 	V[oldsize] = 0;
 
-	for(i = 1; i < 256; i++)
-		if(buckets[i] == buckets[i - 1] +1)
+	for (i = 1; i < 256; i++)
+		if (buckets[i] == buckets[i - 1] +1)
 			I[buckets[i]] = -1;
 
 	I[0] = -1;
 
-	for(h = 1; I[0] != -(oldsize + 1); h += h)
+	for (h = 1; I[0] != -(oldsize + 1); h += h)
 	{
 		len = 0;
 
-		for(i = 0; i < oldsize + 1;)
+		for (i = 0; i < oldsize + 1;)
 		{
-			if(I[i] < 0)
+			if (I[i] < 0)
 			{
 				len -= I[i];
 				i -= I[i];
 			}
 			else
 			{
-				if(len)
+				if (len)
 					I[i - len] = -len;
 
 				len = V[I[i]] + 1 - i;
@@ -195,11 +195,11 @@ static void qsufsort(off_t *I, off_t *V, uint8_t *old, off_t oldsize)
 			}
 		}
 
-		if(len)
+		if (len)
 			I[i - len] = -len;
 	}
 
-	for(i = 0; i < oldsize + 1; i++)
+	for (i = 0; i < oldsize + 1; i++)
 		I[V[i]] = i;
 }
 
@@ -207,8 +207,8 @@ static off_t matchlen(uint8_t *old, off_t oldsize, uint8_t *new, off_t newsize)
 {
 	off_t i;
 
-	for(i = 0; (i < oldsize) && (i < newsize); i++)
-		if(old[i] != new[i])
+	for (i = 0; (i < oldsize) && (i < newsize); i++)
+		if (old[i] != new[i])
 			break;
 
 	return i;
@@ -225,7 +225,7 @@ static off_t search(off_t *I, uint8_t *old, off_t oldsize,
 		x = matchlen(old + I[st], oldsize - I[st], new, newsize);
 		y = matchlen(old + I[en], oldsize - I[en], new, newsize);
 
-		if(x > y)
+		if (x > y)
 		{
 			*pos = I[st];
 			return x;
@@ -462,7 +462,7 @@ int main(int argc, char *argv[])
 					if (new[scan - lenb + i] == old[pos-lenb+i]) 
 						s--;
 
-					if(s > Ss)
+					if (s > Ss)
 					{
 						Ss = s;
 						lens = i + 1;
@@ -473,9 +473,9 @@ int main(int argc, char *argv[])
 				lenb -= lens;
 			}
 
-			for(i = 0; i < lenf; i++)
+			for (i = 0; i < lenf; i++)
 				db[dblen + i] = new[lastscan + i] - old[lastpos + i];
-			for(i = 0; i < (scan - lenb) - (lastscan + lenf); i++)
+			for (i = 0; i < (scan - lenb) - (lastscan + lenf); i++)
 				eb[eblen + i] = new[lastscan + lenf + i];
 
 			dblen += lenf;
@@ -483,16 +483,19 @@ int main(int argc, char *argv[])
 
 			offtout(lenf, buf);
 			BZ2_bzWrite(&bz2err, pfbz2, buf, 8);
+
 			if (bz2err != BZ_OK)
 				errx(1, "BZ2_bzWrite, bz2err = %d", bz2err);
 
 			offtout((scan - lenb) - (lastscan + lenf), buf);
 			BZ2_bzWrite(&bz2err, pfbz2, buf, 8);
+
 			if (bz2err != BZ_OK)
 				errx(1, "BZ2_bzWrite, bz2err = %d", bz2err);
 
 			offtout((pos - lenb) - (lastpos + lenf), buf);
 			BZ2_bzWrite(&bz2err, pfbz2, buf, 8);
+
 			if (bz2err != BZ_OK)
 				errx(1, "BZ2_bzWrite, bz2err = %d", bz2err);
 
@@ -501,7 +504,9 @@ int main(int argc, char *argv[])
 			lastoffset = pos - scan;
 		}
 	}
+
 	BZ2_bzWriteClose(&bz2err, pfbz2, 0, NULL, NULL);
+
 	if (bz2err != BZ_OK)
 		errx(1, "BZ2_bzWriteClose, bz2err = %d", bz2err);
 
@@ -513,10 +518,14 @@ int main(int argc, char *argv[])
 	/* Write compressed diff data */
 	if ((pfbz2 = BZ2_bzWriteOpen(&bz2err, pf, 9, 0, 0)) == NULL)
 		errx(1, "BZ2_bzWriteOpen, bz2err = %d", bz2err);
+
 	BZ2_bzWrite(&bz2err, pfbz2, db, dblen);
+
 	if (bz2err != BZ_OK)
 		errx(1, "BZ2_bzWrite, bz2err = %d", bz2err);
+
 	BZ2_bzWriteClose(&bz2err, pfbz2, 0, NULL, NULL);
+
 	if (bz2err != BZ_OK)
 		errx(1, "BZ2_bzWriteClose, bz2err = %d", bz2err);
 
@@ -528,18 +537,24 @@ int main(int argc, char *argv[])
 	/* Write compressed extra data */
 	if ((pfbz2 = BZ2_bzWriteOpen(&bz2err, pf, 9, 0, 0)) == NULL)
 		errx(1, "BZ2_bzWriteOpen, bz2err = %d", bz2err);
+
 	BZ2_bzWrite(&bz2err, pfbz2, eb, eblen);
+
 	if (bz2err != BZ_OK)
 		errx(1, "BZ2_bzWrite, bz2err = %d", bz2err);
+
 	BZ2_bzWriteClose(&bz2err, pfbz2, 0, NULL, NULL);
+
 	if (bz2err != BZ_OK)
 		errx(1, "BZ2_bzWriteClose, bz2err = %d", bz2err);
 
 	/* Seek to the beginning, write the header, and close the file */
 	if (fseeko(pf, 0, SEEK_SET))
 		err(1, "fseeko");
+
 	if (fwrite(header, 32, 1, pf) != 1)
 		err(1, "fwrite(%s)", argv[3]);
+
 	if (fclose(pf))
 		err(1, "fclose");
 
