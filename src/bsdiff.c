044dd3801a9565a7a390f004dd0b0c80dd603405
William Woodruff <william@tuffbizz.com>
2014-11-15 17:32:11 -0500

    more reformatting

--- src/bsdiff.c
+++ src/bsdiff.c
@@ -252,40 +252,40 @@ static void offtout(off_t x, uint8_t *buf)
 {
 	off_t y;
 
-	if(x<0)
-		y=-x; 
+	if (x < 0)
+		y = -x; 
 	else
-		y=x;
+		y = x;
 
-	buf[0]=y%256;
-	y-=buf[0];
-	y=y/256;
-	buf[1]=y%256;
-	y-=buf[1];
-	y=y/256;
-	buf[2]=y%256;
-	y-=buf[2];
-	y=y/256;
-	buf[3]=y%256;
-	y-=buf[3];
-	y=y/256;
-	buf[4]=y%256;
-	y-=buf[4];
-	y=y/256;
-	buf[5]=y%256;
-	y-=buf[5];
-	y=y/256;
-	buf[6]=y%256;
-	y-=buf[6];
-	y=y/256;
-	buf[7]=y%256;
+	buf[0] = y % 256;
+	y -= buf[0];
+	y = y / 256;
+	buf[1] = y % 256;
+	y -= buf[1];
+	y = y / 256;
+	buf [2] = y % 256;
+	y -= buf[2];
+	y = y/256;
+	buf[3] = y % 256;
+	y -= buf[3];
+	y = y / 256;
+	buf[4] = y % 256;
+	y -= buf[4];
+	y= y / 256;
+	buf[5] = y % 256;
+	y -= buf[5];
+	y = y / 256;
+	buf[6] = y % 256;
+	y -= buf[6];
+	y = y / 256;
+	buf[7] = y % 256;
 
 
-	if(x<0)
+	if (x < 0)
 		buf[7] |= 0x80;
 }
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	int fd;
 	uint8_t *old, *new;
